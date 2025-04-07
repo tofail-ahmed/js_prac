f7adecb476e98f08a280dfafe94535aa2ae3cc86
Tofail Ahmed Sayem <atofail50@gmail.com>
2025-04-07 22:11:51 +0600

    for...of

--- loop.js
+++ loop.js
@@ -1,3 +1,11 @@
 {
+      const numbers=[22,32,34,43,45,54,56,65,67,76,78,87,89,98,90]
+            for (const i of numbers){
+                if(i>50){
+                  // console.log(i-20)
+                }else{
+                  // console.log(i+100)
+                }
+            }
       
 }
