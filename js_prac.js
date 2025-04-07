410030dfc76fc50fdc59af64a25435fe4d0643d2
Tofail Ahmed Sayem <atofail50@gmail.com>
2025-04-07 22:03:51 +0600

    first commit

---
+++ js_prac.js
@@ -0,0 +1,31 @@
+{
+      const numbers=[22,33,45,56,67,65,98,89];
+      const friends=["noman","mamun","rafiq","sofiq"];
+     const newNumbers= numbers.shift(12229)
+      // console.log(numbers)
+
+      const fruits=["mango","apple","banana","litchi"]
+      if(fruits.includes("ango")){
+            // console.log("mango available")
+      }else{
+            // console.log('no mango, start begging...')
+      }
+
+}
+{
+      const names=["babul","alif","choton"]
+      // console.log(names.indexOf("babul"))
+}
+{
+      const cities=["Dhaka","Chattogram","Sylhet"];
+      cities.push("Rajshahi")
+      if(cities.includes("Rajshahi")){
+            // console.log("Its Rajshahi",cities.indexOf("Rajshahi"))
+      }else{
+            // console.log("Its rajshahi")
+      }
+}
+
+{
+      
+}
