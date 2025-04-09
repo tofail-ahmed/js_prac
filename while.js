0ead9774bc04b365b9ecaff3eeb27927148c78e0
Tofail Ahmed Sayem <atofail50@gmail.com>
2025-04-09 21:46:38 +0600

    ...

--- while.js
+++ while.js
@@ -1,12 +1,23 @@
 {
       let age = 0;
-      while (age < 30) {
-        if (age < 20) {
-          console.log(age, "is a teenage");
+      while (age <= 30) {
+        if (age <= 19) {
+      //     console.log(age, "years old is a teenage");
         } else {
-          console.log(age, "is not teenage anymore");
+      //     console.log(age, "years old is not teenage anymore");
         }
         age++; // increment age to avoid infinite loop
       }
     }
+
+    {
+      let price = 200;
+      while (price <= 300) { // Loop through prices starting from 200
+        if (price % 20== 0) { // Check if the price is odd
+          console.log(price, "USD is a reasonable price");
+        }
+        price++;
+      }
+    }
+    
     
