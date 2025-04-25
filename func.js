064f73b9a4bb1d102b92bc9ec2a36e91a5d6b902
Tofail Ahmed Sayem <atofail50@gmail.com>
2025-04-25 20:57:33 +0600

    func prac added

---
+++ func.js
@@ -0,0 +1,32 @@
+const juiceKhaw=()=>{
+      console.log("juice kha beta")
+}
+// juiceKhaw()
+
+const myRoutine=()=>{
+      console.log("take odu")
+      console.log("perform salah")
+      console.log("ttake bf")
+      console.log("get ready for offc")
+      console.log("go to offc")
+      console.log("reach at offc")
+      console.log("reach at offc")
+}
+
+// myRoutine()
+
+const randomFamily=()=>{
+      console.log("abbu")
+      console.log("ammu")
+      console.log("sayem")
+      console.log("kawsar")
+}
+
+// randomFamily()
+
+const multipleFunc=(param1,param2,param3)=>{
+      const  multiply=param1*param2;
+      if(multiply>param3){
+            console.log(object)
+      }
+}
